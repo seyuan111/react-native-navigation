4e9aa850034e2eb5fccff9625a36937480710129
Sean Yuan <ysean54@yahoo.com>
2024-03-29 17:46:07 -0400

    Navigation

---
+++ screens/About.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const About = () => {
+  return (
+    <View>
+      <Text>About</Text>
+    </View>
+  )
+}
+
+export default About
